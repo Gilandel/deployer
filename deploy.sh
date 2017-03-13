7f3b01b9f5fae682250fda71824eb132f15bca5d
Gilandel <gilles@landel.fr>
2017-03-13 01:03:13 +0100

    Correct path

--- deploy.sh
+++ deploy.sh
@@ -7,17 +7,10 @@ MVN_SETTINGS=${DISTRIBUTION_HOME}/settings.xml
 
 mkdir -p $DISTRIBUTION_HOME
 
-echo "DEPLOYER_URL = $DEPLOYER_URL"
-
 curl $DEPLOYER_URL/pubring.gpg -o $DISTRIBUTION_HOME/pubring.gpg
 curl $DEPLOYER_URL/pushingkey.enc -o $DISTRIBUTION_HOME/pushingkey.enc
 curl $DEPLOYER_URL/secring.gpg -o $DISTRIBUTION_HOME/secring.gpg
 curl $DEPLOYER_URL/settings.xml -o $MVN_SETTINGS
-
-echo "check $MVN_SETTINGS"
-ls -lth $DISTRIBUTION_HOME
-echo "read $MVN_SETTINGS"
-cat $MVN_SETTINGS
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
 	echo "Build and deploy SNAPSHOT"
