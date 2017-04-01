61fbbbf1dd912b69a1ff9e199f106a958a1bba43
Gilandel <gilles@landel.fr>
2017-04-02 00:14:34 +0200

    add cache

--- codacity-coverage-report-install.sh
+++ codacity-coverage-report-install.sh
@@ -1,5 +1,5 @@
 SBT_VERSION=0.13.13
-SBT_DIRECTORY=$HOME/.sbt_bin
+SBT_DIRECTORY=$HOME/sbt
 SBT_HOME=$SBT_DIRECTORY/sbt-launcher-packaging-$SBT_VERSION/bin
 CCR_DIRECTORY=$HOME/ccr
 
@@ -8,11 +8,15 @@ mkdir -p $SBT_DIRECTORY
 curl -L https://dl.bintray.com/sbt/native-packages/sbt/$SBT_VERSION/sbt-$SBT_VERSION.tgz > $SBT_DIRECTORY/sbt-$SBT_VERSION.tgz
 tar -xzvf $SBT_DIRECTORY/sbt-$SBT_VERSION.tgz
 
-# clone and build codacity coverage reporter
-mkdir -p $CCR_DIRECTORY
-git clone https://github.com/codacy/codacy-coverage-reporter $CCR_DIRECTORY
+# clone or pull codacity coverage reporter
+if [ -e "$CCR_DIRECTORY/codacy-coverage-reporter-assembly.jar" ]; then
+	git clone https://github.com/codacy/codacy-coverage-reporter $CCR_DIRECTORY
+	cd $CCR_DIRECTORY
+else
+	cd $CCR_DIRECTORY
+	git pull
+fi
 
 # build codacity coverage reporter
-cd $CCR_DIRECTORY
 $SBT_HOME/sbt assembly
-mv $CCR_DIRECTORY/target/codacy-coverage-reporter-assembly-*.jar $CCR_DIRECTORY/codacy-coverage-reporter-assembly.jar
+mv -f $CCR_DIRECTORY/target/codacy-coverage-reporter-assembly-*.jar $CCR_DIRECTORY/codacy-coverage-reporter-assembly.jar
