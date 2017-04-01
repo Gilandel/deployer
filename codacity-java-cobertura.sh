240c88e0c7ad3eb82aa60694c8b71337591ba191
Gilandel <gilles@landel.fr>
2017-04-01 16:38:39 +0200

    add logs

--- codacity-java-cobertura.sh
+++ codacity-java-cobertura.sh
@@ -2,10 +2,12 @@ VERSION=1.0.13
 LANGUAGE=Java
 INPUT=target/cobertura/coverage.xml
 
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.7:get -DgroupId=com.codacy -DartifactId=codacy-coverage-reporter -Dversion=$VERSION -Dtype=pom
+mvn org.apache.maven.plugins:maven-dependency-plugin:3.0.0:get -DgroupId=com.codacy -DartifactId=codacy-coverage-reporter -Dversion=$VERSION -Dtype=pom
 mkdir lib
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.7:copy-dependencies -f /path/to/m2/repo/com/codacy/codacy-coverage-reporter/$VERSION/codacy-coverage-reporter-$VERSION.pom -DoutputDirectory=./lib
+mvn org.apache.maven.plugins:maven-dependency-plugin:3.0.0:copy-dependencies -f /path/to/m2/repo/com/codacy/codacy-coverage-reporter/$VERSION/codacy-coverage-reporter-$VERSION.pom -DoutputDirectory=./lib
 
-java -cp ./lib com.codacy.CodacyCoverageReporter -l $LANGUAGE -r $INPUT
+ls -lth ./lib
+
+java -cp lib/. com.codacy.CodacyCoverageReporter -l $LANGUAGE -r $INPUT
 
 rm -rf ./lib
