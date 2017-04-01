5f00e8bc999362fc8028e2809c3234668c45d56f
Gilandel <gilles@landel.fr>
2017-04-01 16:28:27 +0200

    build classpath with maven

--- codacity-java-cobertura.sh
+++ codacity-java-cobertura.sh
@@ -2,6 +2,10 @@ VERSION=1.0.13
 LANGUAGE=Java
 INPUT=target/cobertura/coverage.xml
 
-curl -sL http://central.maven.org/maven2/com/codacy/codacy-coverage-reporter/$VERSION/codacy-coverage-reporter-$VERSION.jar > codacy-coverage-reporter.jar
-java -jar codacy-coverage-reporter.jar com.codacy.CodacyCoverageReporter -l $LANGUAGE -r $INPUT
-rm codacy-coverage-reporter.jar
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.7:get -DgroupId=com.codacy -DartifactId=codacy-coverage-reporter -Dversion=$VERSION -Dtype=pom
+mkdir lib
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.7:copy-dependencies -f /path/to/m2/repo/com/codacy/codacy-coverage-reporter/$VERSION/codacy-coverage-reporter-$VERSION.pom -DoutputDirectory=./lib
+
+java -cp ./lib com.codacy.CodacyCoverageReporter -l $LANGUAGE -r $INPUT
+
+rm -rf ./lib
