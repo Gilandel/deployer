39f473440c6b550e83071bc6fa937597c58315fd
Gilandel <gilles@landel.fr>
2017-04-01 14:40:03 +0200

    Add jpm4j script (often unavailable)

---
+++ jpm4j.sh
@@ -0,0 +1,6 @@
+# original: http://repo.jpm4j.org/jsonrpc/2.0/jpm
+curl -sL https://github.com/jpm4j/jpm4j.installers/raw/master/dist/biz.aQute.jpm.run.jar >t.jar
+java -jar t.jar init
+rm t.jar
+
+
