2713c6cb822e61818d86071584cd506b8d90e2c4
Gilles Landel <gilles@landel.fr>
2017-11-05 17:42:02 +0100

    correct install

--- codacity-coverage-reporter-install.sh
+++ codacity-coverage-reporter-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # install JQ a JSON processor
-apt-get install jq
+sudo apt-get install jq
 
 # download the latest version of Codacity reporter
 wget -O ~/codacy-coverage-reporter-assembly.jar $(curl https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r .assets[0].browser_download_url)
