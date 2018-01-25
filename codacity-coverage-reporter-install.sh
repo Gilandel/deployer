1aadb136131e0f65f7b9e04637701f5fe6ef1d8a
Gilles Landel <gilles@landel.fr>
2018-01-25 04:52:42 +0100

    correct text

--- codacity-coverage-reporter-install.sh
+++ codacity-coverage-reporter-install.sh
@@ -6,5 +6,5 @@ apt-get install jq
 # download the latest version of Codacity reporter
 wget -q -O ~/codacy-coverage-reporter-assembly.jar $(curl -s https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r .assets[0].browser_download_url)
 
-# Download mising dependencies
+# Download missing dependencies
 wget -q -O ~/activation.jar https://maven.repository.redhat.com/ga/javax/activation/activation/1.1.1.redhat-5/activation-1.1.1.redhat-5.jar
