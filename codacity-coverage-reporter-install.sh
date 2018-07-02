a9c92021e3279e95ce5d7bdc58ebaa4668174096
Gilles Landel <gilles@landel.fr>
2018-07-02 20:02:15 +0200

    Add logs

--- codacity-coverage-reporter-install.sh
+++ codacity-coverage-reporter-install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # install JQ a JSON processor
-apt-get install jq
+apt install jq
 
 # download the latest version of Codacity reporter
-wget -q -O ~/codacy-coverage-reporter-assembly.jar $(curl -s https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r .assets[0].browser_download_url)
+wget -O ~/codacy-coverage-reporter-assembly.jar $(curl -s https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r .assets[0].browser_download_url)
 
 # Download missing dependencies
-wget -q -O ~/activation.jar https://maven.repository.redhat.com/ga/javax/activation/activation/1.1.1.redhat-5/activation-1.1.1.redhat-5.jar
+wget -O ~/activation.jar https://maven.repository.redhat.com/ga/javax/activation/activation/1.1.1.redhat-5/activation-1.1.1.redhat-5.jar
