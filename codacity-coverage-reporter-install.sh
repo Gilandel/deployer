5bbf102f4265584cbb732ade73183363e8105552
Gilles Landel <gilles@landel.fr>
2018-07-02 20:21:15 +0200

    update script

--- codacity-coverage-reporter-install.sh
+++ codacity-coverage-reporter-install.sh
@@ -3,10 +3,11 @@
 # install JQ a JSON processor
 apt-get -y install jq
 
-# download the latest version of Codacity reporter
+# get the latest version of Codacity reporter
 data=$(curl -s https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest)
-echo $data
-wget -O ~/codacy-coverage-reporter-assembly.jar $(echo $data | jq -r .assets[0].browser_download_url)
 
-# Download missing dependencies
-wget -O ~/activation.jar https://maven.repository.redhat.com/ga/javax/activation/activation/1.1.1.redhat-5/activation-1.1.1.redhat-5.jar
+# display JSON data (check purpose)
+echo $data
+
+# download the jar
+wget -O ~/codacy-coverage-reporter-assembly.jar $(echo $data | jq -r .assets[0].browser_download_url)
--- codacity-coverage-reporter-runner.sh
+++ codacity-coverage-reporter-runner.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Run Codacity reporter
-java -cp "$HOME/codacy-coverage-reporter-assembly.jar:$HOME/activation.jar" com.codacy.CodacyCoverageReporter $*
+java -jar "$HOME/codacy-coverage-reporter-assembly.jar" report $*
